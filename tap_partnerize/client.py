1e3310850365bad4a0e857b2968be3438748701c
Omar Abed <35661883+omarabed15@users.noreply.github.com>
2023-03-23 10:27:36 -0400

    CDT-28: Conversion Items stream with daily pagination.

--- tap_partnerize/client.py
+++ tap_partnerize/client.py
@@ -6,28 +6,81 @@ from pathlib import Path
 from typing import Any, Callable, Iterable
 
 import requests
+import csv
+import logging
+import io
 from singer_sdk.authenticators import BasicAuthenticator
-from singer_sdk.helpers.jsonpath import extract_jsonpath
 from singer_sdk.streams import RESTStream
+from singer_sdk.pagination import BaseAPIPaginator
+from requests import Response
+from singer_sdk.plugin_base import PluginBase as TapBaseClass
+from singer_sdk._singerlib import Schema
+from datetime import datetime, timedelta
 
 _Auth = Callable[[requests.PreparedRequest], requests.PreparedRequest]
 SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 
 
+class DayChunkPaginator(BaseAPIPaginator):
+    """A paginator that increments days in a date range."""
+
+    def __init__(self, start_date: str, increment: int = 1, *args: Any, **kwargs: Any) -> None:
+        super().__init__(start_date)
+        self._value = datetime.strptime(start_date, "%Y-%m-%d")
+        self._end = datetime.today()
+        self._increment = increment
+
+    @property
+    def end_date(self):
+        """Get the end pagination value.
+
+        Returns:
+            End date.
+        """
+        return self._end
+
+    @property
+    def increment(self):
+        """Get the paginator increment.
+
+        Returns:
+            Increment.
+        """
+        return self._increment
+
+    def get_next(self, response: Response):
+        return self.current_value + timedelta(days=self.increment) if self.has_more(response) else None
+
+    def has_more(self, response: Response) -> bool:
+        """Checks if there are more days to process.
+
+        Args:
+            response: API response object.
+
+        Returns:
+            Boolean flag used to indicate if the endpoint has more pages.
+        """
+        return self.current_value < self.end_date
+
+
+def set_none_or_cast(value, expected_type):
+    if value == '' or value is None:
+        return None
+    elif not isinstance(value, expected_type):
+        return expected_type(value)
+    else:
+        return value
+
+
 class PartnerizeStream(RESTStream):
     """Partnerize stream class."""
 
-    # TODO: Set the API's base URL here:
-    url_base = "https://api.mysample.com"
+    url_base = "https://api.partnerize.com"
 
-    # OR use a dynamic url_base:
-    # @property
-    # def url_base(self) -> str:
-    #     """Return the API URL root, configurable via tap settings."""
-    #     return self.config["api_url"]
-
-    records_jsonpath = "$[*]"  # Or override `parse_response`.
-    next_page_token_jsonpath = "$.next_page"  # Or override `get_next_page_token`.
+    @property
+    def next_page_token(self) -> str:
+        """Return the API URL root, configurable via tap settings."""
+        return self.config.get("start_date", "")
 
     @property
     def authenticator(self) -> BasicAuthenticator:
@@ -52,37 +105,10 @@ class PartnerizeStream(RESTStream):
         headers = {}
         if "user_agent" in self.config:
             headers["User-Agent"] = self.config.get("user_agent")
-        # If not using an authenticator, you may also provide inline auth headers:
-        # headers["Private-Token"] = self.config.get("auth_token")
         return headers
 
-    def get_next_page_token(
-        self,
-        response: requests.Response,
-        previous_token: Any | None,
-    ) -> Any | None:
-        """Return a token for identifying next page or None if no more pages.
-
-        Args:
-            response: The HTTP ``requests.Response`` object.
-            previous_token: The previous page token value.
-
-        Returns:
-            The next pagination token.
-        """
-        # TODO: If pagination is required, return a token which can be used to get the
-        #       next page. If this is the final page, return "None" to end the
-        #       pagination loop.
-        if self.next_page_token_jsonpath:
-            all_matches = extract_jsonpath(
-                self.next_page_token_jsonpath, response.json()
-            )
-            first_match = next(iter(all_matches), None)
-            next_page_token = first_match
-        else:
-            next_page_token = response.headers.get("X-Next-Page", None)
-
-        return next_page_token
+    def get_new_paginator(self) -> BaseAPIPaginator:
+        return DayChunkPaginator(start_date=self.config.get("start_date"))
 
     def get_url_params(
         self,
@@ -98,32 +124,21 @@ class PartnerizeStream(RESTStream):
         Returns:
             A dictionary of URL query parameters.
         """
-        params: dict = {}
-        if next_page_token:
-            params["page"] = next_page_token
+        params: dict = {
+            "publisher_id": self.config.get("publisher_id", ""),
+            "ref_conversion_metric_id": "2"
+        }
+
+        next_page_date = datetime.strftime(next_page_token, "%Y-%m-%d")
+        if next_page_date:
+            params["start_date"] = next_page_date
+            end_datetime = datetime.strptime(next_page_date, "%Y-%m-%d") + timedelta(days=1)
+            params["end_date"] = datetime.strftime(end_datetime, "%Y-%m-%d")
+
         if self.replication_key:
             params["sort"] = "asc"
             params["order_by"] = self.replication_key
         return params
-
-    def prepare_request_payload(
-        self,
-        context: dict | None,
-        next_page_token: Any | None,
-    ) -> dict | None:
-        """Prepare the data payload for the REST API request.
-
-        By default, no payload will be sent (return None).
-
-        Args:
-            context: The stream context.
-            next_page_token: The next page index or value.
-
-        Returns:
-            A dictionary with the JSON body for a POST requests.
-        """
-        # TODO: Delete this method if no payload is required. (Most REST APIs.)
-        return None
 
     def parse_response(self, response: requests.Response) -> Iterable[dict]:
         """Parse the response and return an iterator of result records.
@@ -134,18 +149,38 @@ class PartnerizeStream(RESTStream):
         Yields:
             Each record from the source.
         """
-        # TODO: Parse response body and return a set of records.
-        yield from extract_jsonpath(self.records_jsonpath, input=response.json())
+
+        f = io.StringIO(response.text)
+        for count, row in enumerate(csv.DictReader(f)):
+            page_date = row.get("conversion_date")
+            logging.info(f"Retrieved {count+1} records for data chunk {page_date}")
+            yield row
 
     def post_process(self, row: dict, context: dict | None = None) -> dict | None:
         """As needed, append or transform raw data to match expected structure.
-
         Args:
             row: An individual record from the stream.
             context: The stream context.
-
         Returns:
             The updated record dictionary, or ``None`` to skip the record.
         """
-        # TODO: Delete this method if not needed.
+        if row.get("meta_conversion_gross_value") == "undefined" or row.get("meta_item_product_id") == "undefined" or row.get("job_id"):
+            return None
+        row["meta_conversion_gross_value"] = set_none_or_cast(row.get("meta_conversion_gross_value"), float)
+        row["item_value"] = set_none_or_cast(row.get("item_value"), float)
+        row["conversion_lag"] = set_none_or_cast(row.get("conversion_lag"), int)
+        row["meta_conversion_delivery_cost"] = set_none_or_cast(row.get("meta_conversion_delivery_cost"), float)
+        row["creative_type"] = set_none_or_cast(row.get("creative_type"), int)
+        row["item_publisher_commission"] = set_none_or_cast(row.get("item_publisher_commission"), float)
+        row["publisher_commission"] = set_none_or_cast(row.get("publisher_commission"), float)
+        row["value"] = set_none_or_cast(row.get("value"), float)
+        row["quantity"] = set_none_or_cast(row.get("quantity"), int)
+        row["meta_conversion_container_version"] = set_none_or_cast(row.get("meta_conversion_container_version"), int)
+        try:
+            row.pop(None, None)  # Removes values from the row which aren't associated with a key (column)
+            row = {
+                k.replace('-', '_').lower(): v for k, v in row.items()
+            }  # BQ Schema doesn't recognize field names with the '-' character
+        except:
+            raise Exception('Check row')
         return row
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,16 +1,13 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
-import datetime
 
-from singer_sdk.testing import get_tap_test_class
+from singer_sdk.testing import get_tap_test_class, get_standard_tap_tests
 
 from tap_partnerize.tap import TapPartnerize
+from singer_sdk.helpers._util import read_json_file
 
-
-SAMPLE_CONFIG = {
-    "start_date": datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d")
-    # TODO: Initialize minimal tap config
-}
+CONFIG_PATH = ".secrets/config.json"
+SAMPLE_CONFIG = read_json_file(CONFIG_PATH)
 
 
 # Run standard built-in tap tests from the SDK:
@@ -20,4 +17,8 @@ TestTapPartnerize = get_tap_test_class(
 )
 
 
-# TODO: Create additional tests as appropriate for your tap.
+def test_standard_tap_tests():
+    """Run standard tap tests from the SDK."""
+    tests = get_standard_tap_tests(TapPartnerize, config=SAMPLE_CONFIG)
+    for test in tests:
+        test()
